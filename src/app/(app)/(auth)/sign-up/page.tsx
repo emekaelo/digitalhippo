821948cb23e28bbeada533a12babad8a385f53fd
emekaelo <devemekaelo@gmail.com>
2025-02-03 11:55:32 +0100

    feat: reconfigure app to run on express server
    ts-node was causing an error so tsx was used to serve the app with nodemon

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -14,8 +14,8 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/src/lib/validators/account-credentials-validator";
+import { trpc } from "@/src/trpc/client";
 
-// export default function Page() {
 const Page = () => {
   const {
     register,
@@ -24,6 +24,9 @@ const Page = () => {
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
   });
+
+  const { data } = trpc.anyApiRoute.useQuery();
+  console.log(data);
 
   const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
 
@@ -48,46 +51,47 @@ const Page = () => {
               <ArrowRight className="h-4 w-4" />
             </Link>
           </div>
-        </div>
-      </div>
+          <div className="grid gap-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register("email")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.email,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
 
-      <div className="grid gap-6">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="grid gap-2">
-            <div className="grid gap-1 py-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                {...register("email")}
-                className={cn({
-                  "focus-visible:ring-red-500": errors.email,
-                })}
-                placeholder="you@example.com"
-              />
-              {errors?.email && (
-                <p className="text-sm text-red-500">{errors.email.message}</p>
-              )}
-            </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    {...register("password")}
+                    type="password"
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.password,
+                    })}
+                    placeholder="Password"
+                  />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
 
-            <div className="grid gap-1 py-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                {...register("password")}
-                type="password"
-                className={cn({
-                  "focus-visible:ring-red-500": errors.password,
-                })}
-                placeholder="Password"
-              />
-              {errors?.password && (
-                <p className="text-sm text-red-500">
-                  {errors.password.message}
-                </p>
-              )}
-            </div>
-
-            <Button>Sign up</Button>
+                <Button>Sign up</Button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </>
   );
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,53 @@
-// import express from "express";
-// import { nextApp, nextHandler } from "./next-utils";
+// File for self-hosting nextjs app
+
+import express from "express";
+import { nextApp, nextHandler } from "./next-utils";
 // import { getPayloadClient } from "./get-payload";
-//
-// const app = express();
-// const PORT = Number(process.env.PORT) || 3000;
-//
-// const start = async () => {
-//   const payload = await getPayloadClient({
-//     initOptions: {
-//       express: app,
-//       onInit: async (cms) => {
-//         cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
-//       },
-//     },
-//   });
-//
-//   app.use((req, res) => nextHandler(req, res));
-//
-//   nextApp.prepare().then(() => {
-//     payload?.logger.info("Next.js started");
-//
-//     app.listen(PORT, async () => {
-//       payload?.logger.info(
-//         `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
-//       );
-//     });
-//   });
-// };
-//
-// start();
-// TODO: uninstall express and nodemon dependencies
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "@/src/trpc";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+
+const start = async () => {
+  // const payload = await getPayloadClient({
+  //   initOptions: {
+  //     express: app,
+  //     onInit: async (cms) => {
+  //       cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+  //     },
+  //   },
+  // });
+
+  // trpc middleware which handles api requests forwarded from express
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    }),
+  );
+
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    // payload?.logger.info("Next.js started");
+    console.log("Next.js started");
+
+    app.listen(PORT, async () => {
+      console.log(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
+      // payload?.logger.info(
+      //   `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
+      // );
+    });
+  });
+};
+
+start();
 // TODO: remove tsconfig.server.json, server.ts, get-payload.ts,nodemon.json
 // TODO: remove .idea from git
